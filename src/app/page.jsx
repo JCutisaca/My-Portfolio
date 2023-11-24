ec43e2b1c876e39827aaadecbec479946b65fed8
soldier_lucas <3jhoncuco@gmail.com>
2023-11-24 20:00:15 -0300

    in progress

---
+++ src/app/page.jsx
@@ -0,0 +1,77 @@
+"use client"
+import Head from "next/head";
+import About from '@/components/About/About'
+import Language from '@/components/Language/Language'
+import Image from 'next/image'
+import styles from './app.module.css'
+import { motion } from "framer-motion"
+import global_en from '@/config/languages/en/global.json'
+import global_es from '@/config/languages/es/global.json'
+import { useEffect } from "react";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Theme from "@/components/Theme/Theme";
+import { ThemeProvider } from "next-themes";
+import Age from "@/components/Age/Age";
+import LinkedIn from "@/components/LinkedIn/LinkedIn";
+import SpotifyCard from "@/components/Spotify/Spotify";
+
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        global: global_en,
+      },
+      es: {
+        global: global_es,
+      },
+    },
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default function Home() {
+  useEffect(() => {
+    const language = localStorage.getItem("language");
+    i18n.changeLanguage(language);
+  }, []);
+  return (
+    <>
+      <ThemeProvider
+      attribute="class"
+      enableSystem={true}
+      defaultTheme="system"
+      >
+        <Head>
+          <title>Jhonathan Cutisaca | FullStack Developer</title>
+        </Head>
+        <motion.div
+          initial={{ opacity: 0, y: -2000 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 100, duration: 1 }}
+          transition={{ duration: 2.5, delay: 0.5, type: "spring" }}
+          className={styles.container}>
+          <div className={styles.containerCards}>
+            <About></About>
+            <Language></Language>
+            <Theme></Theme>
+            <Age></Age>
+            <LinkedIn></LinkedIn>
+            <SpotifyCard></SpotifyCard>
+            <Language></Language>
+            <Language></Language>
+            <Language></Language>
+            <Language></Language>
+            <Language></Language>
+            <Language></Language>
+          </div>
+        </motion.div>
+      </ThemeProvider>
+    </>
+  )
+}
---
+++ src/components/Theme/Theme.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import styles from './Theme.module.css'
+import { useTheme } from "next-themes";
+
+export default function Theme() {
+  const [checked, setChecked] = useState(false);
+  const { theme, setTheme, resolvedTheme } = useTheme();
+
+  useEffect(() => {
+    if (theme === "system") {
+      if (resolvedTheme === "light") {
+        setChecked(true);
+      } else {
+        setChecked(false);
+      }
+    } else {
+      if (theme === "light") {
+        setChecked(true);
+      } else {
+        setChecked(false);
+      }
+    }
+  }, [theme, resolvedTheme]);
+
+  const handleChange = (e) => {
+    const checked = e.target.checked;
+    if (checked) {
+      setChecked(true);
+      setTheme("light");
+    } else {
+      setChecked(false);
+      setTheme("dark");
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center bg-[#1c053a9c] rounded-3xl relative overflow-hidden col-span-1 aspect-square shadow-sm">
+      <div className={styles["checkbox-wrapper-5"]}>
+        <div className={styles.check}>
+          <input
+            type="checkbox"
+            id="check-5"
+            value={checked}
+            checked={checked}
+            onChange={handleChange}
+            name="theme"
+            aria-label="Theme"
+          />
+          <label name="themelabel" htmlFor="check-5"></label>
+        </div>
+      </div>
+    </div>
+  );
+}
