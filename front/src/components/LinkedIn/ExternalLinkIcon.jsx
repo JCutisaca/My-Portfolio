68a6aed1f9b2f70aa8d170e89099dcdba428f510
soldier_lucas <3jhoncuco@gmail.com>
2023-11-27 22:09:01 -0300

    generated components for icons

---
+++ front/src/components/LinkedIn/ExternalLinkIcon.jsx
@@ -0,0 +1,12 @@
+
+
+export default function ExternalLinkIcon() {
+    return (
+        <div>
+            <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" className="absolute right-4 bottom-4 top-auto md:right-8 md:top-8 text-xl text-white dark:text-[#ffffffaa]" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path d="M19 19H5V5h7V3H5a2 2 0 00-2 2v14a2 2 0 002 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"></path>
+            </svg>
+        </div>
+    )
+}
